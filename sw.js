5139c5af6de4198c8ce0aec229bc230d193d3cd5
iadeniregun1 <iadeniregun1@gmail.com>
2026-09-20 19:49:04 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='unilag-quiz-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
